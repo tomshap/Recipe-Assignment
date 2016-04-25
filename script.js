29fdd92ab8c91368e0fe347cf2da0b4f998e5491
tomshap <tom.shapiro2@gmail.com>
2016-04-24 22:29:04 -0400

    Added files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('img').click(function() {
+        $('img').fadeOut('slow');
+    });
+    $('#text').click(function() {
+        $('#text').toggleClass('highlighted');
+    });
+     $('#text1').click(function() {
+        $('#text1').toggleClass('highlighted');
+    });
+      $('#text2').click(function() {
+        $('#text2').toggleClass('highlighted');
+    });
+      $('#text3').click(function() {
+        $('#text3').toggleClass('highlighted');
+    });
+      $('#text4').click(function() {
+        $('#text4').toggleClass('highlighted');
+    });
+      $('#text5').click(function() {
+        $('#text5').toggleClass('highlighted');
+    });
+      $('#text6').click(function() {
+        $('#text6').toggleClass('highlighted');
+    });
+      $('#text7').click(function() {
+        $('#text7').toggleClass('highlighted');
+    });
+      $('#text8').click(function() {
+        $('#text8').toggleClass('highlighted');
+    });
+      $('#text9').click(function() {
+        $('#text9').toggleClass('highlighted');
+    });
+      $('#text10').click(function() {
+        $('#text10').toggleClass('highlighted');
+    });
+      $('#text11').click(function() {
+        $('#text11').toggleClass('highlighted');
+    });
+});
+
